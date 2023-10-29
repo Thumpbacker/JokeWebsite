143913d26551d1b7e79eae4c5ddc463de9038772
Thumpbacker <Creeperbro81@gmail.com>
2023-10-29 15:09:05 -0700

    a

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost";
+$host = "uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "JokesPart1";
+$username = "v6ammxmq583wesuy";
+$user_pass = "vybkmm7p8cosz1m3";
+$database_in_use = "e6qsexjtcl76zt8t";
 
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
